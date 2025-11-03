0a22db516abbf11e610e355ae846ec80f273eaba
Divya Jagannathan <divya.jaganathan@icts.res.in>
2025-11-03 11:44:05 -0500

    Add files via upload
    This repository contains the Python code used to generate the 2D surface wake pattern for ultra-soft solids in the gravity-capillary-elastic regime using 2D IFFT.

--- soft_wake.py
+++ soft_wake.py
@@ -11,24 +11,18 @@ matplotlib.use('Agg')
 
 # ================================================================
 # Program: 2D Inverse Fourier transform (IFFT) to compute the wake
-#          pattern on ultrasoft solid for given Froude number Fr,
-#          Bond number Bo, and scaled shear wave speed cs.
-# Author: Divya Jagannathan (Harvard University 2025)
+#          pattern on ultrasoft solid for given Froude number (Fr),
+#          Bond number (Bo), scaled shear wave speed (cs), and  
+#          number of spectral modes (nk).
+# Author: Divya Jagannathan (2025)
 # ===============================================================
 
+# ============================
+# ---USER-DEFINED FUNCTIONS--- 
+# ============================
 
-def mycolor():
-    basic = plt.get_cmap('seismic')
-    bright_red = (1.0, 0.0, 0.0, 1.0)
-    colors = [bright_red, bright_red, basic(0.5), basic(0.25), basic(0.25)]
-    nodes = [0, 0.25, 0.5, 0.75, 1]
-    mycmap = mc.LinearSegmentedColormap.from_list(
-        'rwb_stretched', list(zip(nodes, colors)))
-    return mycmap
-
-
+# *** Computes integrand in eq. 14 ***
 def integrand(kx, ky, Fr, Bo, cs):
-    # --- Computes Integrand in Eq. 14 ---
     c1 = 1.0
     c2 = Fr*cs
     c3 = 1/Bo**2
@@ -36,16 +30,15 @@ def integrand(kx, ky, Fr, Bo, cs):
     eps = 1e-2
     k = np.sqrt(kx**2 + ky**2)
     f1 = np.exp(-((k*L)/(2*np.pi))**2)
-    # Regularization of Doppler Shift
-    dpplr = (Fr*kx + 1j*eps)
+    dpplr = (Fr*kx + 1j*eps)  # (regularisation)
     f2_num = k*(dpplr**2)
     f2_den = (dpplr**2)*(c1*k + c3*k**3) - (dpplr**2 - 2*(c2*k)
                                             ** 2)**2 + 4*(c2*k)**3*np.sqrt((c2*k)**2 - dpplr**2)
     f2 = f2_num/f2_den
-    fhatk = -f1*f2
+    fhatk = f1*f2
     return fhatk
 
-
+# *** Returns locations of surface elevation extrema along diff ridges ***
 def get_peaks(X, Y, W, l_ridges):
     x_peaks, y_peaks, z_peaks = [], [], []
     for label_id in np.unique(l_ridges):
@@ -62,7 +55,8 @@ def get_peaks(X, Y, W, l_ridges):
         z_peaks.append(W[row[0], col[0]])
     return np.array(x_peaks), np.array(y_peaks), np.array(z_peaks)
 
-
+# *** Identifies ridges per some thresholds, and returns elevation field (W_new) 
+#     and orientation (orientation_max) of the leading ridge ***
 def get_arms(X, Y, W, Lxy, cs, extra_dom):
     wake_ht_min = 1e-4
     min_strength_ridge = 1e-2
@@ -103,14 +97,16 @@ def get_arms(X, Y, W, Lxy, cs, extra_dom):
     W_new = np.where(ridge_mask, W_pos, 0)
     return ridge_mask, W_new, orientation_max
 
-
+# *** Plots surface wake (top-view) behind the source in stationary frame ***
 def plot_wake_3d(X, Y, W, Lxy, Fr, cs, Nk, extra_dom, nunits):
     Lmbda = 2*np.pi*Fr**2
     l_ridges, Wsub, wangle_r = get_arms(X, Y, W, Lxy, cs, extra_dom)
+
     # Straight line (xl, yl) with slope of the leading dominant ridge
     xl = np.arange(-4*Lmbda, Lmbda, Lmbda)
     yl = np.tan(wangle_r)*xl
     wangle = -np.degrees(wangle_r)
+    
     # Plotting the figure
     fig, ax = plt.subplots(1, 1, figsize=(8, 6))
     p = ax.pcolormesh(X, Y, W, cmap=mycolor(),
@@ -134,7 +130,7 @@ def plot_wake_3d(X, Y, W, Lxy, Fr, cs, Nk, extra_dom, nunits):
     plt.close(fig)
     return wangle
 
-
+# *** auxilliary functions ***
 def setup_axis(ax, Lxy, Lmbda,  extra_dom, nunits, with_labels=False):
     ax.set_xlim(-0.75*Lxy/extra_dom, 0.25*Lxy/extra_dom)
     ax.set_ylim(-Lxy/(2*extra_dom), Lxy/(2*extra_dom))
@@ -157,6 +153,15 @@ def save_data(X, Y, W, Fr, Bo, cs, wangle, k, if_save_k):
         np.savetxt(f"wavenumbers_Fr{Fr}_cs{cs:.3f}.dat",
                    np.array(k), delimiter=",")
 
+def mycolor():
+    basic = plt.get_cmap('bwr')
+    bright_red = (1.0, 0.0, 0.0, 1.0)
+    bright_blue = (0.0, 0.0, 1.0, 1.0)
+    colors = [bright_blue, bright_blue, basic(0.5), bright_red, bright_red]
+    nodes = [0, 0.25, 0.5, 0.75, 1]
+    mycmap = mc.LinearSegmentedColormap.from_list(
+        'rwb_stretched', list(zip(nodes, colors)))
+    return mycmap
 
 # User-defined Parameters
 parser = argparse.ArgumentParser()
@@ -182,8 +187,7 @@ nunits = 6
 Lxy = extra_domain*(nunits*2*np.pi*Fr**2)
 
 # Construct the discrete spectral space
-# (NOTE: k used in the integral should be radians per unit length; hence multiplied by 2*pi)
-kx = 2*np.pi*np.fft.fftfreq(Nk, d=Lxy/Nk)
+kx = 2*np.pi*np.fft.fftfreq(Nk, d=Lxy/Nk) # (angular wavenumers)
 ky = kx
 KX, KY = np.meshgrid(kx, ky, indexing='ij')
 
@@ -199,7 +203,6 @@ fpad = np.fft.ifftshift(padded_shift)
 
 # Compute the 2D ifft to get the surface wave height
 fxy = np.fft.ifft2(fpad)
-# (!!) CAUTION: Taking real part after performing the inverse Fourier transform
 wake = np.real(np.fft.fftshift(fxy))
 norm_wake = wake/np.max(np.abs(wake))
 
@@ -219,4 +222,6 @@ wAngle = plot_wake_3d(X, Y, shifted_wake, Lxy, Fr,
 save_k = 0  # (save_k = 1 if you want to save the wavenumbers used in the ifft)
 save_data(X, Y, shifted_wake, Fr, Bo, cs,  wAngle, kx, save_k)
 
-# ---THE END---#
+# =============
+# ---THE END---
+# =============
